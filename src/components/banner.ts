96e5d5f1c38fba75468bdf5b41225858bb78b27a
Lucas Garcia <fr.lcsgrc@gmail.com>
2026-01-06 14:05:17 +0100

    feat: add the maternity leave info banner

---
+++ src/components/banner.ts
@@ -0,0 +1,44 @@
+import { css, html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('ui-banner')
+export class Banner extends LitElement {
+  static styles = css`
+    :host {
+      display: grid;
+      grid-template-columns: auto 1fr;
+      gap: var(--size-2);
+      padding: var(--size-2);
+      align-items: center;
+
+      background: color-mix(in srgb, var(--color-tertiary), white 90%);
+      border: var(--border-size-1) solid var(--color-tertiary);
+      border-radius: var(--radius-3);
+      corner-shape: squircle;
+      color: var(--color-tertiary);
+    }
+
+    svg {
+      inline-size: var(--size-4);
+    }
+  `;
+
+  protected render() {
+    return html`
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="currentColor"
+      >
+        <path
+          d="M480-280q17 0 28.5-11.5T520-320v-160q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480v160q0 17 11.5 28.5T480-280Zm0-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+        />
+      </svg>
+
+      <slot></slot>
+    `;
+  }
+}
